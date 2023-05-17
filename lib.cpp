ea6f5d104fb5ea392270139a6993a8404b33acd1
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-05-17 19:06:09 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,24 @@
+#include "lib.h";
+bool verifica (char b){
+    int a;
+    a=(int)b;
+    if (((a>=65)&&(a<=90))or((a>=97)&&(a<=122))) {
+
+        return true;
+    }else {
+        return false;
+    }
+}
+
+char conversione (char b) {
+    char risultato;
+    int a;
+    a= int(b);
+    if ((a>=65)&&(a<=90)) {
+        risultato= char(a+32);
+    }else if ((a>=97)&&(a<=122)){
+        risultato =char(a-32);
+    }
+    return risultato;
+}
 
